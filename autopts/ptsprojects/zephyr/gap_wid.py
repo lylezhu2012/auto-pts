c288b3e3df0991c3b65a701a4fbaee630e992020
Lyle Zhu <lyle.zhu@nxp.com>
2023-11-08 11:20:07 +0800

    [MCUX-64364] Support GAP group test cases
    - Support cases
    GAP/MOD/NDIS/BV-01-C
    GAP/MOD/LDIS/BV-01-C
    GAP/MOD/LDIS/BV-02-C
    GAP/MOD/LDIS/BV-03-C
    GAP/MOD/GDIS/BV-01-C
    GAP/MOD/GDIS/BV-02-C
    GAP/MOD/NCON/BV-01-C
    GAP/MOD/CON/BV-01-C
    GAP/MOD/NBON/BV-02-C
    GAP/MOD/NBON/BV-03-C
    GAP/SEC/SEM/BV-02-C
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/gap_wid.py
+++ autopts/ptsprojects/zephyr/gap_wid.py
@@ -14,9 +14,12 @@
 #
 
 import logging
+from time import sleep
 
 from autopts.wid import generic_wid_hdl
-from autopts.pybtp.types import WIDParams
+from autopts.ptsprojects.stack import get_stack
+from autopts.pybtp import btp, defs
+from autopts.pybtp.types import WIDParams, UUID, gap_settings_btp2txt, AdType, AdFlags, IOCap
 
 log = logging.debug
 
@@ -39,4 +42,54 @@ def hdl_wid_162(_: WIDParams):
 
 
 def hdl_wid_224(_: WIDParams):
+    return True
+
+# Please make IUT not discoverable. Press OK to continue.
+def hdl_wid_31(_: WIDParams):
+    btp.gap_set_nonconn()
+    return True
+
+def hdl_wid_32(_: WIDParams):
+    btp.gap_set_limdiscov()
+    return True
+
+def hdl_wid_33(params: WIDParams):
+    if params.test_case_name.startswith("GAP/MOD/NBON/BV-03-C"):
+        btp.gap_set_io_cap(IOCap.no_input_output)
+    if params.test_case_name.startswith("GAP/SEC/SEM/BV-02-C"):
+        btp.gap_set_io_cap(IOCap.keyboard_display)
+        btp.core_reg_svc_l2cap()
+        btp.l2cap_listen('0x1001', defs.L2CAP_TRANSPORT_BREDR, 120)
+    btp.gap_set_gendiscov()
+    return True
+
+def hdl_wid_34(_: WIDParams):
+    btp.gap_set_nonconn()
+    return True
+
+def hdl_wid_145(_: WIDParams):
+    timeout = 0
+    while True:
+        stack = get_stack()
+        if not stack.gap.current_settings_get(
+            gap_settings_btp2txt[defs.GAP_SETTINGS_DISCOVERABLE]):
+            return True
+        else:
+            sleep(5)
+            timeout += 5
+        if timeout >= 60:
+            break
+    return False
+
+def hdl_wid_105(_: WIDParams):
+    btp.gap_set_conn()
+    btp.gap_set_gendiscov()
+    return True
+
+def hdl_wid_160(_: WIDParams):
+    btp.gap_set_limdiscov()
+    return True
+
+def hdl_wid_222(_: WIDParams):
+    btp.gap_pair()
     return True
